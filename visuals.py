4e243790fc848e4a67bef3c8f4e989a649709ff9
Jean-Luc COSSI <jeanluc@cossi.biz>
2019-07-20 18:00:08 +0200

    fix: improve visualizations layout

--- visuals.py
+++ visuals.py
@@ -60,7 +60,8 @@ def evaluate(results, accuracy, f1):
     """
   
     # Create figure
-    fig, ax = pl.subplots(2, 3, figsize = (11,7))
+    fig, ax = pl.subplots(2, 3, figsize = (13,9))
+    pl.subplots_adjust(wspace=0.4, hspace=0.4)
 
     # Constants
     bar_width = 0.3
@@ -110,12 +111,12 @@ def evaluate(results, accuracy, f1):
     patches = []
     for i, learner in enumerate(results.keys()):
         patches.append(mpatches.Patch(color = colors[i], label = learner))
-    pl.legend(handles = patches, bbox_to_anchor = (-.80, 2.53), \
+    pl.legend(handles = patches, bbox_to_anchor = (-.80, 2.8), \
                loc = 'upper center', borderaxespad = 0., ncol = 3, fontsize = 'x-large')
     
     # Aesthetics
-    pl.suptitle("Performance Metrics for Three Supervised Learning Models", fontsize = 16, y = 1.10)
-    pl.tight_layout()
+    pl.suptitle("Performance Metrics for Three Supervised Learning Models", fontsize = 16, y = 1.08)
+    #pl.tight_layout()
     pl.show()
     
 
@@ -127,7 +128,7 @@ def feature_plot(importances, X_train, y_train):
     values = importances[indices][:5]
 
     # Creat the plot
-    fig = pl.figure(figsize = (9,5))
+    fig = pl.figure(figsize = (13,5))
     pl.title("Normalized Weights for First Five Most Predictive Features", fontsize = 16)
     pl.bar(np.arange(5), values, width = 0.6, align="center", color = '#00A000', \
           label = "Feature Weight")
@@ -140,4 +141,4 @@ def feature_plot(importances, X_train, y_train):
     
     pl.legend(loc = 'upper center')
     pl.tight_layout()
-    pl.show()  
+    pl.show() 
